78ddde8fbae409d51a25c3c20ffba46764b59bf7
Josh Pieper <jjp@pobox.com>
2021-03-02 10:42:14 -0500

    Remove most firmware support for silk r3.x boards

--- fw/power_dist.cc
+++ fw/power_dist.cc
@@ -1,4 +1,4 @@
-// Copyright 2019-202 Josh Pieper, jjp@pobox.com.
+// Copyright 2019-2021 Josh Pieper, jjp@pobox.com.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -43,9 +43,6 @@ void Store(T& out, T value) {
   std::memcpy(&out, &value, sizeof(value));
 }
 
-const uint32_t kPrechargeMs = 100;
-const float kMaxPrechargedVoltage = 10.0f;
-
 enum State {
   kPowerOff,
   kPrecharging,
@@ -53,51 +50,6 @@ enum State {
   kFault,
 };
 
-#if POWER_DIST_HW_REV < 1
-void SetClock0() {
-  // RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-
-  // Select HSI as system clock source and configure the HCLK, PCLK1
-  // and PCLK2 clocks dividers.
-  RCC_ClkInitStruct.ClockType = (
-      RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 |
-      RCC_CLOCKTYPE_PCLK2);
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1; // 16 MHz
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;  // 16 MHz
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;  // 16 MHz
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK) {
-    return;
-  }
-
-  /** Configure the main internal regulator output voltage
-   */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE2);
-
-  {
-    RCC_PeriphCLKInitTypeDef PeriphClkInit = {};
-
-    PeriphClkInit.PeriphClockSelection =
-        RCC_PERIPHCLK_FDCAN |
-        RCC_PERIPHCLK_I2C2 |
-        RCC_PERIPHCLK_ADC12 |
-        RCC_PERIPHCLK_ADC345;
-    PeriphClkInit.FdcanClockSelection = RCC_FDCANCLKSOURCE_PCLK1;
-    PeriphClkInit.Adc12ClockSelection = RCC_ADC12CLKSOURCE_SYSCLK;
-    PeriphClkInit.Adc345ClockSelection = RCC_ADC345CLKSOURCE_SYSCLK;
-    PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
-    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-    {
-      mbed_die();
-    }
-  }
-
-  SystemCoreClockUpdate();
-}
-#endif
-
-#if POWER_DIST_HW_REV >= 2
 void SetClock2() {
   RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
@@ -131,214 +83,7 @@ void SetClock2() {
 
   SystemCoreClockUpdate();
 }
-#endif
 
-#if POWER_DIST_HW_REV < 1
-void RunRev0() {
-  fw::FDCan::Filter filters[] = {
-    { 0x10005, 0xffffff, fw::FDCan::FilterMode::kMask,
-      fw::FDCan::FilterAction::kAccept,
-      fw::FDCan::FilterType::kExtended,
-    },
-  };
-
-  fw::FDCan can(
-      [&]() {
-        fw::FDCan::Options options;
-        options.td = CAN_TX;
-        options.rd = CAN_RX;
-        options.slow_bitrate = 125000;
-        options.fast_bitrate = 125000;
-
-        options.filter_begin = &filters[0];
-        options.filter_end = &filters[0] + (sizeof(filters) / sizeof(filters[0]));
-
-        return options;
-      }());
-
-  DigitalOut led1(DEBUG_LED1, 1);
-  DigitalOut led2(DEBUG_LED2, 0);
-
-  DigitalOut switch_led(PWR_LED);
-  DigitalIn power_switch(PWR_SW, PullDown);
-
-  DigitalOut fet_precharge(FET_PRECHARGE);
-  DigitalOut fet_main(FET_MAIN);
-
-  // We use this merely to configure the pins.
-  AnalogIn vsamp_out_do_not_use(VSAMP_OUT);
-
-  fw::MillisecondTimer timer;
-
-  State state = kPowerOff;
-  uint32_t precharge_start = 0;
-
-  uint32_t last_can = 0;
-
-  char can_status_data[8] = {
-    0, // switch status
-    0, // lock time in 0.1s
-  };
-
-  char can_command_data[8] = {};
-
-  char& power_switch_status = can_status_data[0];
-  uint8_t& lock_time = reinterpret_cast<uint8_t&>(can_status_data[1]);
-
-  FDCAN_RxHeaderTypeDef can_rx_header;
-
-  // Configure ADC5
-  {
-    // Disable it to ensure we are in a known state.
-    if (ADC5->CR & ADC_CR_ADEN) {
-      ADC5->CR |= ADC_CR_ADDIS;
-      while (ADC5->CR & ADC_CR_ADEN);
-    }
-    ADC345_COMMON->CCR = 0;  // no divisor
-    ADC5->CR &= ~ADC_CR_DEEPPWD;
-    ADC5->CR |= ADC_CR_ADVREGEN;
-    timer.wait_us(20);
-    ADC5->CR |= ADC_CR_ADCAL;
-    while (ADC5->CR & ADC_CR_ADCAL);
-    timer.wait_us(1);
-
-    ADC5->ISR |= ADC_ISR_ADRDY;
-    ADC5->CR |= ADC_CR_ADEN;
-    while (!(ADC5->ISR & ADC_ISR_ADRDY));
-
-    ADC5->ISR |= ADC_ISR_ADRDY;
-    ADC5->CFGR &= ~(ADC_CFGR_CONT);
-    ADC5->CFGR2 &= ~(
-        ADC_CFGR2_SMPTRIG |
-        ADC_CFGR2_BULB |
-        ADC_CFGR2_SWTRIG |
-        ADC_CFGR2_GCOMP |
-        ADC_CFGR2_ROVSM |
-        ADC_CFGR2_TROVS |
-        ADC_CFGR2_JOVSE |
-        ADC_CFGR2_ROVSE |
-        0);
-
-
-    const int sqr = 1;  // PA8 is channel 1
-    ADC5->SQR1 =
-        (0 << ADC_SQR1_L_Pos) | // length 1
-        (sqr << ADC_SQR1_SQ1_Pos);
-    auto make_cycles = [](auto v) {
-      return
-        (v << 0) |
-        (v << 3) |
-        (v << 6) |
-        (v << 9) |
-        (v << 12) |
-        (v << 15) |
-        (v << 18) |
-        (v << 21) |
-        (v << 24);
-    };
-    ADC5->SMPR1 = make_cycles(2);  // 12 ADC cycles
-    ADC5->SMPR2 = make_cycles(2);
-  }
-
-
-  for (;;) {
-    {
-      const auto now = timer.read_ms() / 100;
-      if (now != last_can) {
-        last_can = now;
-
-        can.Send(0x10004, std::string_view(can_status_data,
-                                           sizeof(can_status_data)));
-
-        if (lock_time > 0) { lock_time--; }
-      }
-
-      if (can.Poll(&can_rx_header, can_command_data)) {
-        if (can_command_data[1] > 0) {
-          lock_time = static_cast<uint8_t>(can_command_data[1]);
-        }
-      }
-    }
-
-    switch (state) {
-      case kPowerOff: {
-        switch_led.write(1);  // inverted
-        fet_main.write(0);
-        fet_precharge.write(0);
-        break;
-      }
-      case kPrecharging: {
-        switch_led.write((timer.read_ms() / 20) % 2);
-        fet_main.write(0);
-        fet_precharge.write(1);
-        break;
-      }
-      case kPowerOn: {
-        switch_led.write(0);
-        fet_main.write(1);
-        fet_precharge.write(1);
-        break;
-      }
-      case kFault: {
-        fet_main.write(0);
-        fet_precharge.write(0);
-        switch_led.write((timer.read_ms() / 200) % 2);
-        break;
-      }
-    }
-
-    power_switch_status = (power_switch.read() == 0) ? 0 : 1;
-
-    // Sample the ADC.
-    ADC5->CR |= ADC_CR_ADSTART;
-    while ((ADC5->ISR & ADC_ISR_EOC) == 0);
-    const uint16_t raw_adc = ADC5->DR;
-    const float out_voltage =
-        (static_cast<float>(raw_adc) /
-         4096.0f * 3.3f) / VSAMP_DIVIDE;
-
-    // Handle state transitions.
-    if (power_switch_status == 1) {
-      // The switch is on.  Try to get into the power on state.
-      switch (state) {
-        case kPowerOff: {
-          precharge_start = timer.read_ms();
-          state = kPrecharging;
-          break;
-        }
-        case kPrecharging: {
-          const uint32_t elapsed_ms = timer.read_ms() - precharge_start;
-          // Either move to power on or fault.  We fault if the
-          // voltage did not get low enough during the pre-charge
-          // cycle.
-          if (elapsed_ms > kPrechargeMs) {
-            if (out_voltage > kMaxPrechargedVoltage) {
-              state = kFault;
-            } else {
-              state = kPowerOn;
-            }
-          }
-          break;
-        }
-        case kPowerOn: {
-          // Nothing to do here.
-          break;
-        }
-        case kFault: {
-          break;
-        }
-      }
-    } else {
-      if (lock_time == 0) {
-        // Only turn off once our lock time has expired.
-        state = kPowerOff;
-      }
-    }
-  }
-}
-#endif
-
-#if POWER_DIST_HW_REV >= 1
 class OpAmpInvertingAmplifier {
  public:
   OpAmpInvertingAmplifier(OPAMP_TypeDef* opamp) {
@@ -911,8 +656,6 @@ void RunRev2() {
     }
   }
 }
-#endif
-
 }
 
 ADC_TypeDef* const g_adc5 = ADC5;
@@ -921,11 +664,7 @@ volatile uint32_t rcc_csr = 0;
 int main(void) {
   rcc_csr = RCC->CSR;
 
-#if POWER_DIST_HW_REV < 1
-  // Drop our speed down to nothing, because we don't really need to
-  // go fast for this and we might as well save the battery.
-  SetClock0();
-#elif POWER_DIST_HW_REV >= 2
+#if POWER_DIST_HW_REV >= 2
   SetClock2();
 #else
 #error "Unsupported target"
@@ -964,9 +703,7 @@ int main(void) {
 
 
 
-#if POWER_DIST_HW_REV < 1
-  RunRev0();
-#elif POWER_DIST_HW_REV >= 2
+#if POWER_DIST_HW_REV >= 2
   RunRev2();
 #else
 #error "Unsupported target"
--- fw/power_dist_hw.h
+++ fw/power_dist_hw.h
@@ -1,4 +1,4 @@
-// Copyright 2018-2020 Josh Pieper, jjp@pobox.com.
+// Copyright 2018-2021 Josh Pieper, jjp@pobox.com.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,19 +16,20 @@
 
 // r2 and r3 silk is hw rev 0
 // r4.0, r4.1 silk is hw rev 1
-// r4.2 is hw rev 2
+// r4.2 is hw rev 2, but is undistributed
+// r4.3, r4.3b is hw rev 2
 
 
 // The most recent version of the HW.
 #ifndef POWER_DIST_HW_REV
-// r4.2 silk
+// r4.3b silk
 #define POWER_DIST_HW_REV 2
 #endif
 
 constexpr int kHardwareInterlock[] = {
   0,   // r2, r3
   1,   // r4.0, r4.1
-  2,   // r4.2
+  2,   // r4.3, r4.3b
 };
 
 // This firmware is compatible with the following hardware revisions.
@@ -40,58 +41,21 @@ constexpr int kCompatibleHwRev[] = {
 #define HWREV_PIN1 PA_15
 #define HWREV_PIN2 PC_13
 
-#if POWER_DIST_HW_REV < 1
-#define DEBUG_LED1 PA_4
-#define DEBUG_LED2 PA_5
-#else
 #define DEBUG_LED1 PA_0
 #define DEBUG_LED2 PA_1
-#endif
 
-#if POWER_DIST_HW_REV < 1
-#define FET_PRECHARGE PA_0
-#define FET_MAIN PA_1
-#endif
-
-#if POWER_DIST_HW_REV < 1
-#define PWR_LED PA_2
-#define PWR_SW PA_3
-#else
 #define PWR_LED PB_12
 #define PWR_SW PA_2
-#endif
 
-#if POWER_DIST_HW_REV < 1
-#define VSAMP_IN PA_9
-#define VSAMP_OUT PA_8
-#else
 #define VSAMP_IN PB_14
 #define VSAMP_OUT PA_7
-#endif
 
 #define CAN_TX PA_12
 #define CAN_RX PA_11
-#if POWER_DIST_HW_REV < 1
 #define CAN_SHDN PA_10
-#elif POWER_DIST_HW_REV >= 2
-#define CAN_SHDN PA_10
-#else
-#error "Unsupported target"
-#endif
 
 
-#if POWER_DIST_HW_REV < 1
-// Only reworked boards r2/r3/r3.1 boards have this divider, but
-// non-reworked versions have an older firmware installed.
-#define VSAMP_DIVIDE (4.7f / (10.0f + 100.0f))
-#else
 #define VSAMP_DIVIDE (200.0f / (200.0f + 3000.0f))
-#endif
-
-#if POWER_DIST_HW_REV < 1
-// Nothing here.
-
-#elif POWER_DIST_HW_REV >= 2
 
 #define GPIO1 PC_10
 #define GPIO2 PC_11
@@ -107,7 +71,3 @@ constexpr int kCompatibleHwRev[] = {
 #define TPS2490_FLT PA_5
 #define TSP2490_TIMER PB_11
 #define FET_TEMP PC_5
-
-#else
-#error "Unsupported target"
-#endif
